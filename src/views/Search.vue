5197839c95ab8ca5c1a4bd4642554c3781f99d03
va2ron1 <va2ron1@hotmail.com>
2019-07-23 01:33:32 -0400

    search page done

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,16 +2,40 @@
   <b-row>
     <b-col cols="12">
       <b-row>
-        <b-col md="6" class="my-1" v-bind:class="{ 'mx-auto': items.length === 0 }">
-          <b-form-group label-cols-sm="3" label="Search">
-            <b-input-group>
-              <b-form-input v-model="search" placeholder="Type to Search"></b-form-input>
-              <b-input-group-append>
-                <b-button v-if="items.length === 0" :disabled="!search" @click="doSearch()">Search</b-button>
-                <b-button v-else :disabled="!search" @click="search = ''">Clear</b-button>
-              </b-input-group-append>
-            </b-input-group>
-          </b-form-group>
+        <b-col
+          md="6"
+          class="my-1"
+          v-bind:class="{ 'mx-auto': items.length === 0 }"
+        >
+          <b-row>
+            <b-col md="10">
+              <b-form-group label-cols-sm="3" label="Search">
+                <b-input-group>
+                  <b-form-input
+                    v-model="search"
+                    placeholder="Type to Search"
+                    v-on:keyup.enter="doSearch()"
+                  ></b-form-input>
+                  <b-input-group-append>
+                    <b-button
+                      v-if="items.length === 0"
+                      :disabled="!search"
+                      @click="doSearch()"
+                      >Search</b-button
+                    >
+                    <b-button v-else :disabled="!search" @click="search = ''"
+                      >Clear</b-button
+                    >
+                  </b-input-group-append>
+                </b-input-group>
+              </b-form-group>
+            </b-col>
+            <b-col md="2" v-if="isLoading">
+              <div class="text-center d-flex justify-content-between">
+                <b-spinner variant="secondary"></b-spinner>
+              </div>
+            </b-col>
+          </b-row>
         </b-col>
       </b-row>
 
@@ -27,7 +51,8 @@
         </b-table>
         <b-row class="justify-content-center justify-content-md-start">
           <div class="my-1">
-            <b-pagination variant="danger"
+            <b-pagination
+              variant="danger"
               v-model="currentPage"
               :total-rows="totalRows"
               :per-page="perPage"
@@ -42,14 +67,16 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
 
 @Component({})
 export default class Search extends Vue {
-  items = [] as Array<object>
-  totalRows = 4
-  currentPage = 1
-  perPage = 4
-  search = ""
+  items = [] as Array<object>;
+  totalRows = 4;
+  currentPage = 1;
+  perPage = 4;
+  search = "";
+  isLoading = false;
 
   mounted() {
     // Set the initial number of items
@@ -57,21 +84,37 @@ export default class Search extends Vue {
   }
 
   doSearch() {
-    this.items = []
-    this.totalRows = this.items.length;
+    this.isLoading = true;
+    axios
+      .get(
+        "https://api.datar.online/v1/data/" +
+          process.env.VUE_APP_DATAR_API_KEY +
+          "?search=" +
+          this.search
+      )
+      .then(response => {
+        this.isLoading = false;
+        this.items = response.data.data;
+        this.totalRows = this.items.length;
+      })
+      .catch(e => {
+        this.isLoading = false;
+        this.items = [{ data: "Nothing has been found with your search." }];
+        this.totalRows = this.items.length;
+      });
   }
 }
 </script>
 <style>
 .page-item.active .page-link {
-    background-color: #6c757d !important;
-    border-color: #6c757d !important;
-    color: #fff !important;
+  background-color: #6c757d !important;
+  border-color: #6c757d !important;
+  color: #fff !important;
 }
 .page-link {
   color: #000 !important;
 }
 .page-item.disabled .page-link {
-    color: #e5e5e5 !important;
+  color: #e5e5e5 !important;
 }
 </style>
