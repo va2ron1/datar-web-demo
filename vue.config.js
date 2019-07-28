6a643594b1d912e3958f5776e57d7b4b525cb5c5
va2ron1 <va2ron1@hotmail.com>
2019-07-28 16:22:42 -0400

    caching api

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,23 @@
 // vue.config.js
+module.exports = {
+  devServer: {
+    host: "demo.datar.com"
+  },
+  pwa: {
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      navigateFallback: "/index.html",
+      runtimeCaching: [{
+        urlPattern: new RegExp('https://api.datar.online/v1/data'),
+        handler: 'networkFirst',
+        options: {
+          networkTimeoutSeconds: 10,
+          cacheName: 'datar-api-cache',
+          cacheableResponse: {
+            statuses: [200],
+          },
+        },
+      }]
+    }
+  }
+};
